75f2c4ade54b93867b63968425de525e38071f25
strix190788 <strix190788@gmail.com>
2026-03-15 09:09:30 +0400

    Update moveFunctions.ino

--- moveFunctions.ino
+++ moveFunctions.ino
@@ -1,5 +1,20 @@
-void lineMove(int pwr, float kp){
+void lineMove(int pwr, float kp) {
   int error = leftSens.mapRead() - rightSens.mapRead();
+  int Psost = error * kp;
+  bot.move(1.05 * pwr - Psost, pwr + Psost);
+}
+
+void lineMove(int pwr, float kp, int prd) {
+  long currentMillis = millis();
+  while (millis() - currentMillis < prd) lineMove(pwr, kp);
+}
+
+void driveToCross(int pwr, float kp) {
+  while (leftSens.mapRead() + rightSens.mapRead() < 80) lineMove(pwr, kp);
+  //while (leftSens.mapRead() < 50 or rightSens.mapRead() < 50) lineMove(pwr, kp);
+}
+
+void pMove(int error, int pwr, float kp){
   int Psost = error * kp;
   bot.move(pwr - Psost, pwr + Psost);
 }
